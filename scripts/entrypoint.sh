f931734cbb34c691c2293327e3059dfcc6adcef5
SX <50966843+S-S-X@users.noreply.github.com>
2023-07-27 23:15:09 +0300

    Almost no hacks :)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while read -r ENV; do export -- "$ENV"; done < <(env | grep -- '-[^=]*=' | sed -r ':ape s/^([^-=]*)-/\1_/;tape;s/^([^=]*=)(.*)$/\1"\2"/')
+while read -r ENV; do export -- "$ENV"; done < <(env | grep -- '-[^=]*=' | sed -r ':ape s/^([^-=]*)-/\1_/;tape')
 
 cd "${INPUT_WORKING_DIRECTORY}"
 
